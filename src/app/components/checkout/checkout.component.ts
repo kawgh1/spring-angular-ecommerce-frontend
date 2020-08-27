37a05ff5ddbe5db7c13035fb6843f0f31556e329
thrwwy216 <thrwwy216@tutanota.com>
2020-08-27 12:26:36 -0500

    added state, country classes, impl dynamic state assignment by country in checkout form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShopFormService } from 'src/app/services/shop-form.service';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +16,26 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  //countries
+  countries: Country[] = [];
+
+
+  // need 2 separate states array for shipping and billing
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+
+
+
+  constructor(private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService) { }
+
+
+
+
 
   ngOnInit(): void {
 
@@ -54,12 +76,68 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+
+
+
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+
+      data => {
+
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+
+
+    // populate credit card years
+    this.shopFormService.getCreditCardYears().subscribe(
+
+      data => {
+
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+
+
+    // populate countries from JPA when form is initially displayed
+    this.shopFormService.getCountries().subscribe(
+
+      // data rec'd back from API
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+
+        // set checkout component property 'countries' to = the returned countries API data
+        this.countries = data;
+      }
+    )
   }
 
+
+
+
+
   onSubmit() {
+
+    // logging
+
     console.log("handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
+
+    console.log("The customer email address is " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The customer shipping state is " + this.checkoutFormGroup.get('shippingAddress').value.state);
+    console.log("The customer shipping country is " + this.checkoutFormGroup.get('shippingAddress').value.country);
   }
+
+
 
 
   copyShippingAddressToBillingAddress(event) {
@@ -75,4 +153,80 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+
+
+
+
+
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if current year equals selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+
+      startMonth = 1; // January for any other year than current year
+    }
+
+    // get the data from shopFormService
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+
+      data => {
+
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+
+
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    // the API only requires the country code "IN", "CA" to return states
+    // http://localhost:8080/api/states/search/findByCountryCode?code=DE
+
+    // returning the name here for future debugging purposes
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+
+    this.shopFormService.getStates(countryCode).subscribe(
+
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+
+          this.shippingAddressStates = data;
+
+        }
+        else {
+
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
+  }
 }
+
---
+++ src/app/services/shop-form.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopFormService {
+
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+
+
+
+  constructor(private httpClient: HttpClient) { }
+
+
+
+
+
+
+  // our angular components will subscribe to these methods and 
+  // receive this async data
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build array for "Month" drop down list
+    // - start at current month and loop until
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+
+      data.push(theMonth);
+    }
+
+    // the 'of' operator from rxjs will wrap an object as an observable
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build array for "Year" drop down list
+    // - start at current yera and loop for next 5 years
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+
+      data.push(theYear);
+    }
+
+    // the 'of' operator from rxjs will wrap an object as an observable
+    return of(data);
+  }
+
+
+
+
+
+
+  // makes Spring Data REST API call and unwraps the countries from
+  // a JSON data package returned
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+
+      // map method imported from rxjs/operators
+      map(response => response._embedded.countries)
+    );
+  }
+
+
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    // search url, retrieve states based on the country code -> "IN", "US", "CA"
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+
+      // map method imported from rxjs/operators
+      map(response => response._embedded.states)
+    );
+  }
+
+
+}
+
+
+
+
+
+// this interface unwraps the JSON data from Spring Data REST _embedded entry
+interface GetResponseCountries {
+
+  _embedded: {
+
+    countries: Country[];
+
+  }
+}
+
+// this interface unwraps the JSON data from Spring Data REST _embedded entry
+interface GetResponseStates {
+
+  _embedded: {
+
+    states: State[];
+
+  }
+}
